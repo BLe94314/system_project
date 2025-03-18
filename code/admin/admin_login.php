6e5bb2c77d24d4dc472a5604523020a035b08a3b
Bryant Le <lbryant053@gmail.com>
2025-03-18 17:05:55 -0400

    Update admin_login.php

--- code/admin/admin_login.php
+++ code/admin/admin_login.php
@@ -49,48 +49,48 @@ session_start();
             </ul>
         </div>
     </nav>
-        <div class="col-md-12" id="main_content">
+    <div class="col-md-12" id="main_content">
+        <center>
+            <h3><u>Admin Login Form</u></h3>
+        </center>
+        <form action="" method="post">
+            <div class="form-group">
+                <label for="email">Email ID:</label>
+                <input type="text" name="email" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" name="password" class="form-control" required>
+            </div><br>
             <center>
-                <h3><u>Admin Login Form</u></h3>
-            </center>
-            <form action="" method="post">
-                <div class="form-group">
-                    <label for="email">Email ID:</label>
-                    <input type="text" name="email" class="form-control" required>
+                <div class="button-group">
+                    <button type="submit" name="login" class="btn btn-primary">Login</button>
                 </div>
-                <div class="form-group">
-                    <label for="password">Password:</label>
-                    <input type="password" name="password" class="form-control" required>
-                </div><br>
-                <center>
-                    <div class="button-group">
-                        <button type="submit" name="login" class="btn btn-primary">Login</button>
-                    </div>
-                </center>
-            </form>
-            <?php
-            if (isset($_POST['login'])) {
-                $connection = mysqli_connect("localhost", "root", "");
-                $db = mysqli_select_db($connection, "lms");
-                $query = "select * from admins where email = '$_POST[email]'";
-                $query_run = mysqli_query($connection, $query);
-                while ($row = mysqli_fetch_assoc($query_run)) {
-                    if ($row['email'] == $_POST['email']) {
-                        if ($row['password'] == $_POST['password']) {
-                            $_SESSION['name'] = $row['name'];
-                            $_SESSION['email'] = $row['email'];
-                            header("Location: admin_dashboard.php");
-                        } else {
-                            ?>
-                            <br><br>
-                            <center><span class="alert-danger">Wrong Password !!</span></center>
-                            <?php
-                        }
+            </center>
+        </form>
+        <?php
+        if (isset($_POST['login'])) {
+            $connection = mysqli_connect("localhost", "root", "");
+            $db = mysqli_select_db($connection, "lms");
+            $query = "select * from admins where email = '$_POST[email]'";
+            $query_run = mysqli_query($connection, $query);
+            while ($row = mysqli_fetch_assoc($query_run)) {
+                if ($row['email'] == $_POST['email']) {
+                    if ($row['password'] == $_POST['password']) {
+                        $_SESSION['name'] = $row['name'];
+                        $_SESSION['email'] = $row['email'];
+                        header("Location: admin_dashboard.php");
+                    } else {
+                        ?>
+                        <br><br>
+                        <center><span class="alert-danger">Wrong Password !!</span></center>
+                        <?php
                     }
                 }
             }
-            ?>
-        </div>
+        }
+        ?>
+    </div>
     </div>
 </body>
 
